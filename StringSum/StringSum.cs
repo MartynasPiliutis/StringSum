ff522a1fc55e4d84adb63004de0897d86b8dce30
Martynas Piliutis <martynas_piliutis@epam.com>
2025-05-29 12:35:37 +0300

    Developed Sum method
    Sum method developed to pass ReturnZeroIfTheStringIsEmptyOrNull test.

--- StringSum/StringSum.cs
+++ StringSum/StringSum.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace StringSum
 {
-    public class StringSum(string? num1, string? num2)
+    public class StringSum(string num1, string num2)
     {
-        private readonly string? num1 = num1;
-        private readonly string? num2 = num2;
+        private readonly string num1 = num1;
+        private readonly string num2 = num2;
 
         internal string Sum(string? num1, string? num2)
         {
-            throw new NotImplementedException();
+            string sum = "0";
+            if (string.IsNullOrEmpty(num1) && string.IsNullOrEmpty(num2))
+            {
+                return sum;
+            }
+
+            return sum;
         }
     }
 }
