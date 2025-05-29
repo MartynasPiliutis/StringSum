df1318205efc7138387bff6765f24574eacc4a26
Martynas Piliutis <martynas_piliutis@epam.com>
2025-05-29 12:07:24 +0300

    First TDD test case
    Started with a simplest test case with an empty string

---
+++ StringSum/StringSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSum
+{
+    public class StringSum(string? num1, string? num2)
+    {
+        private readonly string? num1 = num1;
+        private readonly string? num2 = num2;
+
+        internal string Sum(string? num1, string? num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- StringSum/UnitTests.cs
+++ StringSum/UnitTests.cs
@@ -2,15 +2,23 @@ namespace StringSum
 {
     public class Tests
     {
+        private StringSum _stringSum;
+
         [SetUp]
         public void Setup()
         {
+            string num1 = "";
+            string num2 = "";
+
+            _stringSum = new StringSum(num1, num2);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase("", "", ExpectedResult = "0")]
+        [TestCase("", null, ExpectedResult = "0")]
+        [TestCase(null, null, ExpectedResult = "0")]
+        public string ReturnZeroIfTheStringIsEmptyOrNull(string? num1, string? num2)
         {
-            Assert.Pass();
+            return _stringSum.Sum(num1, num2);
         }
     }
 }
